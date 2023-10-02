8869d5abff803d6fac0ba0841711674ef6af4e93
Robert Michaud <guildwarsguy123@yahoo.com>
2023-10-02 19:22:33 -0400

    fixing login image

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './AuthPage.module.css';
 import SignUpForm from "../../components/SignupForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import logoImage from "../../../main2.png";
 
 function AuthPage(props) {
   return (
     <main className={styles.main}>
-      <img className={styles.mainimage} src="main2.png" alt="Fake Store Logo" />
+      <img src={logoImage} alt="Logo" className={styles.mainImage} />
       <p className={styles.textstyle}>Sign Up:</p>
       <SignUpForm className ={styles.form} setUser={props.setUser} />
       <p className={styles.textstyle}>Log In:</p>
